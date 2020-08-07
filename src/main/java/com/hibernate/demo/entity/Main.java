2ca35e4ded1238d1a675a47a24797626dcaac7a9
Arx1971 <adnan.rahin13@gmail.com>
2020-08-07 14:01:23 -0400

    MySQL database Added

---
+++ src/main/java/com/hibernate/demo/entity/Main.java
@@ -0,0 +1,29 @@
+package com.hibernate.demo.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+
+        Session session = factory.getCurrentSession();
+
+        try {
+            Student student = new Student("Megatron", "Wall", "megatron@email.com");
+            session.beginTransaction();
+            session.save(student);
+            session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
+
+    }
+
+
+}
