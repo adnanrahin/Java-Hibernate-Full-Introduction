2e4475d48761a46c788fac468897b95a78becb53
Arx1971 <adnan.rahin13@gmail.com>
2020-08-10 15:30:55 -0400

    Remove Instructor Hibernate

---
+++ src/main/java/com/hibernate/demo/mappings/onetoone/unidirectional/DeleteInstructor.java
@@ -0,0 +1,35 @@
+package com.hibernate.demo.mappings.onetoone.unidirectional;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteInstructor {
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration().configure("hibernate1.cfg.xml").addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            int instructorId = 2;
+
+            Instructor instructor = session.get(Instructor.class, instructorId);
+
+            System.out.println(instructor);
+
+            session.delete(instructor);
+
+            session.getTransaction().commit();
+        }finally {
+            sessionFactory.close();
+        }
+
+
+    }
+
+}
