bf8c37b495c5da29ef58c2d0cb27dcc0984bc324
Arx1971 <adnan.rahin13@gmail.com>
2020-08-24 09:31:07 -0400

    Fetch Type Update

---
+++ src/main/java/com/hibernate/demo/mappings/fetching/eager/RetrieveInstructor.java
@@ -0,0 +1,38 @@
+package com.hibernate.demo.mappings.fetching.eager;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class RetrieveInstructor {
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration().configure("hibernate2.cfg.xml").addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Instructor instructor = session.get(Instructor.class, 1);
+
+            System.out.println("Instructor: " + instructor);
+
+            System.out.println("Course: " + instructor.getCourses());
+
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+
+    }
+
+}
