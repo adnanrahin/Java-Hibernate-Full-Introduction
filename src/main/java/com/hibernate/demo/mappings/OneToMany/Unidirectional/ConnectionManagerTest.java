96727e6f1b24ab6dc63cb6b64933beb83fecde78
Arx1971 <adnan.rahin13@gmail.com>
2020-08-26 19:03:46 -0400

    Hibernate Query For Retrieve Data

---
+++ src/main/java/com/hibernate/demo/mappings/OneToMany/Unidirectional/ConnectionManagerTest.java
@@ -0,0 +1,16 @@
+package com.hibernate.demo.mappings.OneToMany.Unidirectional;
+
+public class ConnectionManagerTest {
+
+    public static void main(String[] args) {
+
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+
+    }
+}
