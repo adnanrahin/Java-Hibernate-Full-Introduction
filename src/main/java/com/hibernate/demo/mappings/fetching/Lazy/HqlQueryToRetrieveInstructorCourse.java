aec7b48f148ad1ef80e30e7334fc8cf7d56bf0dd
Arx1971 <adnan.rahin13@gmail.com>
2020-08-26 17:42:03 -0400

    HQL query To load data

---
+++ src/main/java/com/hibernate/demo/mappings/fetching/Lazy/HqlQueryToRetrieveInstructorCourse.java
@@ -0,0 +1,46 @@
+package com.hibernate.demo.mappings.fetching.Lazy;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+public class HqlQueryToRetrieveInstructorCourse {
+
+    public static void main(String[] args) {
+
+        SessionFactory sessionFactory = new Configuration().configure("hibernate2.cfg.xml").addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+
+            session.beginTransaction();
+
+            int theId = 1;
+
+            Query<Instructor> query = session.createQuery("select i from Instructor i "
+                            + "JOIN fetch i.courses " + "where i.id =: theInstructorId",
+                    Instructor.class);
+
+            query.setParameter("theInstructorId", theId);
+
+            Instructor instructor = query.getSingleResult();
+
+            System.out.println(instructor.getCourses());
+
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+            sessionFactory.close();
+        }
+
+    }
+
+}
