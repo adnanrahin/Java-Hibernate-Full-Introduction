23145521e63607e1db185637bd4fb9f282e0206b
Arx1971 <adnan.rahin13@gmail.com>
2020-08-28 10:18:00 -0400

    Connection Established

--- src/main/java/com/hibernate/demo/mappings/OneToMany/Unidirectional/ConnectionManagerTest.java
+++ src/main/java/com/hibernate/demo/mappings/OneToMany/Unidirectional/ConnectionManagerTest.java
@@ -1,10 +1,21 @@
 package com.hibernate.demo.mappings.OneToMany.Unidirectional;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class ConnectionManagerTest {
 
     public static void main(String[] args) {
 
         try {
+
+            String url = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false";
+            String userName = "hbstudent";
+            String password = "hbstudent";
+
+            Connection connection = DriverManager.getConnection(url, userName, password);
+
+            System.out.println(connection.getMetaData());
 
         } catch (Exception e) {
             e.printStackTrace();
